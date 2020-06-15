44c2d767bc7aae2bb2071c06d7bb97e310fa6d60
denisGreen <kolomiitsev.denis@gmail.com>
2020-06-15 18:07:43 +0300

    new build version

---
+++ precache-manifest.59516d452973cad57792becc0f257c98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "959569a85c0c8c80b452f663211477e6",
+    "url": "/color-game-build-version/index.html"
+  },
+  {
+    "revision": "fada054a7feef3200c7b",
+    "url": "/color-game-build-version/static/css/main.86c6ee7a.chunk.css"
+  },
+  {
+    "revision": "7fe67a12756f262f25e6",
+    "url": "/color-game-build-version/static/js/2.94e503d2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/color-game-build-version/static/js/2.94e503d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fada054a7feef3200c7b",
+    "url": "/color-game-build-version/static/js/main.f6ecd20d.chunk.js"
+  },
+  {
+    "revision": "66987dd5ce9d1f7cc68b",
+    "url": "/color-game-build-version/static/js/runtime-main.f5b7aadd.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/denisGreen/color-game-build-version/precache-manifest.c8c12b46cfe88e1f745d1d354faf88f2.js"
+  "/color-game-build-version/precache-manifest.59516d452973cad57792becc0f257c98.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/denisGreen/color-game-build-version/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/color-game-build-version/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
